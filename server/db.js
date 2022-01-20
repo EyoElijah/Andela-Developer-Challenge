ac27e2032a1634e15e6f5e677ebf6ca6fc0f09f7
EYO@DESKTOP-8MSK2NT <eyoelijah@gmail.com>
2022-01-20 13:20:55 +0100

    modify database configuration

--- server/db.js
+++ server/db.js
@@ -3,10 +3,10 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: process.env.user,
-  host: process.env.host,
-  database: process.env.database,
-  password: process.env.password,
+  user: process.env.dbuser,
+  host: process.env.dbhost,
+  database: process.env.dbdatabase,
+  password: process.env.dbpassword,
   port: process.env.port,
 });
 
